503104d0fa1e61ac341ee03c111262dbfcf3128e
Siege333 <celiacreaghan@gmail.com>
2022-02-11 14:22:15 +0000

    addition of Day1Packages file to repository

---
+++ src/com/qa/week1/Day1Packages.java
@@ -0,0 +1,20 @@
+package com.qa.week1; //Package definition must be the top line
+
+//Import required packages and classes below the package definition line. 
+//This means they are ready for use when defining/implementing methods and functions
+
+import java.util.Scanner; 	//This imports a single class ("Scanner") from the pre-built "util" Java package
+import java.sql.*;			//This imports all the classes from the pre-built Java package "sql"
+
+
+public class Day1Packages {
+
+	public static void main(String[] args) {
+		
+		//package naming convention - use lower case letters.
+		//package     domain.companyname.packagename;
+		//e.g. package  com.      qa.		week1;
+		//see above at line 1.
+	}
+}
+
